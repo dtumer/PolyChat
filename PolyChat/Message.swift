cb69171df2561effeeed395a7a9471548d83910b
Deniz Tumer <e.tumer.deniz@gmail.com>
2016-08-19 13:51:04 -0700

    added code for Message.swift

--- PolyChat/Message.swift
+++ PolyChat/Message.swift
@@ -7,3 +7,47 @@
 //
 
 import Foundation
+
+class Message {
+    var id: String = ""
+    var body: String = ""
+    var senderId: String = ""
+    var numLikes: Int = 0
+    var numDislikes: Int = 0
+    
+    init(dictionary: NSDictionary) {
+        if let id = dictionary["id"] as? String {
+            self.id = id
+        }
+        else if let id = dictionary["id"] as? Int {
+            self.id = String(id)
+        }
+        
+        if let body = dictionary["body"] as? String {
+            self.body = body
+        }
+        
+        if let senderId = dictionary["sender_id"] as? String {
+            self.senderId = senderId
+        }
+        
+        if let numLikes = dictionary["num_likes"] as? Int {
+            self.numLikes = numLikes
+        }
+        
+        if let numDislikes = dictionary["num_dislikes"] as? Int {
+            self.numDislikes = numDislikes
+        }
+    }
+    
+    //Converts this instance of ChatRoom to JSON object
+    func toDictionary() -> NSDictionary {
+        return [
+            //"id": self.id,
+            "body": self.body,
+            "sender_id": self.senderId,
+            "num_likes": self.numLikes,
+            "num_dislikes": self.numDislikes
+        ]
+    }
+}
