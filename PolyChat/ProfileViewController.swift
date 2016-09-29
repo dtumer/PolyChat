3b39bd1d387c0bd4268c12123e1d0e69070ac45c
Stefan Bonilla <stefanbon7@gmail.com>
2016-09-28 21:58:17 -0700

    Added storyboard navigation and templates for side menu navigation items: My Courses, Profile, Settings

---
+++ PolyChat/ProfileViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ProfileViewController.swift
+//  PolyChat
+//
+//  Created by Stefan Bonilla on 9/28/16.
+//  Copyright © 2016 DenFan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+    
+    //services variables
+    var authService: AuthServiceProtocol!
+    
+    //logged in user
+    var user: User!
+    
+    //on view did load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initServices()
+        
+        //makes sure there's no weird grayness happening in the nav bar
+        self.navigationController?.navigationBar.translucent = false
+    }
+    
+    //initializes all services needed by this controller
+    private func initServices() {
+        self.authService = AuthServiceFactory.sharedInstance
+    }
+    
+    //initializes the slide out menu
+    private func initMenu() {
+        if (self.revealViewController() != nil) {
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            let menuVC = self.revealViewController().rearViewController as! MainMenuTableViewController
+            menuVC.user = self.user
+        }
+    }
+    
+    //on view did appear
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //check if a user is logged in
+        if !authService.hasOpenSession() {
+            self.performSegueWithIdentifier(Constants.loginSegueId, sender: self)
+        }
+        else {
+            //get logged in user information
+            self.authService.getCurrentUser({ (user, error) in
+                if let error = error {
+                    //TODO log error
+                    print(error.description)
+                }
+                else {
+                    self.user = user!
+                    // Menu needs user information since it varies based on user (username, role, etc.)
+                    self.initMenu()
+                }
+            })
+        }
+    }
+
+}
